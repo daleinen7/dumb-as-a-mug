e461c9e88d93c9ae27601d0e61c537b12d977394
daleinen7 <daleinen@gmail.com>
2021-06-12 17:22:59 -0500

    update homepage to include links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,23 @@
 import * as React from "react"
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import styled from 'styled-components';
+import { navigate } from "gatsby";
 
 const StyledParagraph = styled.p`
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   color: #525029;
+
+  a {
+    color: #4f4a00; 
+    text-decoration: underline;
+  }
 `;
 
 const IndexPage = ({location}) => {
   return (
     <Layout  location={location}>
-      <StyledParagraph>Dumb as a Mug is a webcomic that ran from 2011-2012.</StyledParagraph>
+      <StyledParagraph>Dumb as a Mug is a webcomic that ran from 2011-2012 by <a href="https://namorrison7.wixsite.com/mysite/home">Neal Morrison</a>. <Link to='/0'>Start At the Beginning!</Link></StyledParagraph>
     </Layout>
   )
 }
